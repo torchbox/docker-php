a3f7ba4e6cccaf77a376c7dd967a4ddac3b0029d
Dan Slinky <swissslinky@gmail.com>
2016-04-17 15:10:44 +0100

    reduce image size, build logs and gitignore

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 INAME="torchbox/php"
-TAGS="5.4-fpm 5.5-fpm 5.5-fpm 7-fpm"
-
+TAGS="5.4-fpm 5.5-fpm 5.6-fpm 7-fpm"
+mkdir -p logs
 for T in $TAGS; do
-  echo "Building ${INAME}:${T}..."
-  CONTAINER=$(docker build -q -t ${INAME}:$T -f Dockerfile.$T .)
-  echo "Built ${INAME}:$T using ${CONTAINER}" 
+  echo -n "Building ${INAME}:${T}... "
+  CONTAINER=$(docker build --no-cache --rm -t ${INAME}:$T -f Dockerfile.$T . > logs/$T.log 2>&1)
+  echo $CONTAINER
 done
-
-
